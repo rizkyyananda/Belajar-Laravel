a2983e21cfd78666e131dcedf5c5ad9a6b6b6872
Rizky Yananda <yanandarizky@gmail.com>
2019-10-21 09:25:00 +0700

    add to github

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+@section('title','About')
+@section('container')
+</nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-2">{{$name}}</h1>
+            </div>
+        </div>
+    </div>
+    <div class="h1"></div>
+@endsection
